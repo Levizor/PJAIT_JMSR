24fb1f1efb301b8841c9b371e40f445cb0c26e69
Levizor <levizorri@gmail.com>
2025-01-05 18:11:20 +0100

    Fin

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,8 @@ import javax.script.ScriptException;
 import javax.swing.*;
 import java.awt.*;
 
-public class Main extends JFrame {
-
-    JPanel opts;
-    JPanel view;
-
-    public Main(String name) {
-        super(name);
-        opts=new JPanel();
-        view=new JPanel();
-        opts.add(new JButton("Exit"));
-        view.add(new JButton("Exit"));
-        add(opts, BorderLayout.WEST);
-        add(view, BorderLayout.CENTER);
-        setSize(800, 600);
-        setLocationRelativeTo(null);
-    }
-
-    public void run(){
-        setVisible(true);
-    }
-    public static void main(String[] args) throws ScriptException {
-        new Main("Analyzer").run();
-//        ScriptEngine engine=new ScriptEngineManager().getEngineByName("groovy");
-//        engine.put("x", 1);
-//        engine.eval("x = x+2");
-//        int x = (int) engine.get("x");
-//        System.out.println(x);
+public class Main{
+    public static void main(String[] args) {
+        GUI.main(args);
     }
 }
